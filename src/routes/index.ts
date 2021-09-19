19a1652954c77c11d10b1db7de780bc7d48622ff
Ken Nguyen <kenn.tnguyen@gmail.com>
2021-09-19 17:24:54 -0400

    Debug deploy

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,5 +9,8 @@ module.exports = function (app: Express) {
     res.send('<h1>API RUNNING!</h1>');
   });
 
+  app.get('*', (req: Request, res:Response) => {
+    res.send('Page NOT-FOUND 404: This will be developed soon. Please contact admin if you need help')
+  })
   //other routes..
 };
