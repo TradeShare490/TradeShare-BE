0193c98930ff9778ddf303303bd40406265e3dac
Ken Nguyen <kenn.tnguyen@gmail.com>
2021-09-19 16:37:29 -0400

    updated api

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,7 @@
 import { Express, Request, Response } from 'express';
 
 module.exports = function (app: Express) {
-  app.get('/', (req: Request, res: Response) => {
+  app.get('/api/check', (req: Request, res: Response) => {
     res.send('<h1>Hello from the TypeScript world!</h1>');
   });
 
