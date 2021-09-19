aaa601d8f4cb09fe9b248b5d964109d40fe6c149
Ken Nguyen <kenn.tnguyen@gmail.com>
2021-09-19 16:58:59 -0400

    Debug deploy

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,12 @@
 import { Express, Request, Response } from 'express';
 
 module.exports = function (app: Express) {
-  app.get('/api/check', (req: Request, res: Response) => {
+  app.get('/', (req: Request, res: Response) => {
     res.send('<h1>Hello from the TypeScript world!</h1>');
+  });
+
+  app.get('/api/check', (req: Request, res: Response) => {
+    res.send('<h1>API RUNNING!</h1>');
   });
 
   //other routes..
