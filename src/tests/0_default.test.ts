495d7c9d07037bd44d9075e0123cf08154eaa5af
Ken Nguyen <kenn.tnguyen@gmail.com>
2021-10-05 12:07:51 -0400

    [TS-76] debugging, added new db for testing

--- src/tests/0_default.test.ts
+++ src/tests/0_default.test.ts
@@ -1,5 +1,29 @@
 import { expect } from "chai";
 import DefaultService from "../services/default.service";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Setup MongoDB connection
+before(async () => {
+    const mongoUrl = process.env.TEST_DB_URI || "mongodb+srv://ken:5ta9cHYDPIRS6PMa@cluster0.wva5y.mongodb.net/TradeShareTestDB?retryWrites=true&w=majority";
+    if (!mongoUrl) {
+        console.log("ERROR: Please check if mongodb url exists in your .env file");
+        throw new Error("MONGODB_URL not found. Please check if mongodb url exists in your .env file");
+    } else {
+        await mongoose
+            .connect(mongoUrl)
+            .then((res) => console.log("CONNECT TO TEST DB SUCCESSFULLY"))
+            .catch((err) => {
+                console.log(err.message);
+                throw new Error(err.message);
+            });
+    }
+});
+
+after(async () => {
+    await mongoose.disconnect();
+});
 
 describe('Default service can', () => {
     let service:DefaultService;
--- src/tests/4_user.test.ts
+++ src/tests/4_user.test.ts
@@ -1,27 +1,10 @@
 import { expect } from "chai";
-import mongoose from "mongoose";
-import { ILoginUser } from "../db/models/LoginUser.model";
+import { LoginUser } from "../db/models/LoginUser.model";
 import UserService from "../services/User.service";
 
-describe.only("User service can", () => {
-	let mockedUser: ILoginUser;
+describe("User service can", () => {
+	let mockedUser: LoginUser;
 	let userService: UserService;
-
-	before(async () => {
-		const mongoUrl = process.env.MONGODB_URL || "mongodb+srv://damienleu:tradeshare@cluster0.s6elo.mongodb.net/tradesharedb?retryWrites=true&w=majority";
-		if (!mongoUrl) {
-			console.log("ERROR: Please check if mongodb url exists in your .env file");
-			throw new Error("MONGODB_URL not found");
-		} else {
-			await mongoose
-				.connect(mongoUrl)
-				.then((res) => console.log("CONNECT TO DB SUCCESSFULLY"))
-				.catch((err) => {
-					console.log(err.message);
-					throw new Error(err.message);
-				});
-		}
-	});
 
 	it("be setup", () => {
 		userService = new UserService();
@@ -29,7 +12,7 @@ describe.only("User service can", () => {
 	});
 
 	it("create a new user", async () => {
-		const input = { email: "ken", password:"1234" };
+		const input = { email: "ken", password: "1234" };
 		let response: any;
 		try {
 			response = await userService.createUser(input);
@@ -45,28 +28,28 @@ describe.only("User service can", () => {
 		mockedUser = response.user;
 	});
 
-	it('get the user by id', async () => {
-		const res = await userService.getUser({id: mockedUser._id})
-		expect(res.success).to.be.true 
-		expect(res).to.have.property('user')
-	
-		expect(res.user._id).not.to.equal('undefined')
-	})
+	it("get the user by id", async () => {
+		const res = await userService.getUser({ id: mockedUser._id });
+		expect(res.success).to.be.true;
+		expect(res).to.have.property("user");
 
-	it('get the user by username', async () => {
-		const res = await userService.getUser({email: mockedUser.email})
-		expect(res.success).to.be.true 
-		expect(res).to.have.property('user')
-		expect(res.user.email).to.equal(mockedUser.email)
-	})
+		expect(res.user._id).not.to.equal("undefined");
+	});
 
-	it('update user profile by id', async () => {
-		const updateInput = {password: '4567'}
-		const res = await userService.updateUser(mockedUser._id, updateInput)
-		expect(res.success).to.be.true 
+	it("get the user by username", async () => {
+		const res = await userService.getUser({ email: mockedUser.email });
+		expect(res.success).to.be.true;
+		expect(res).to.have.property("user");
+		expect(res.user.email).to.equal(mockedUser.email);
+	});
+
+	it("update user profile by id", async () => {
+		const updateInput = { password: "4567" };
+		const res = await userService.updateUser(mockedUser._id, updateInput);
+		expect(res.success).to.be.true;
 		expect(res.user).to.have.property("password");
 		expect(res.user.password).to.equal(updateInput.password);
-	})
+	});
 
 	it("delete a user by id", async () => {
 		let response: any;
@@ -78,9 +61,5 @@ describe.only("User service can", () => {
 		expect(response).not.equal("undefined");
 		expect(response).to.have.property("deletedCount");
 		expect(response.deletedCount).to.equal(1);
-	});
-
-	after(async () => {
-		await mongoose.disconnect();
 	});
 });
