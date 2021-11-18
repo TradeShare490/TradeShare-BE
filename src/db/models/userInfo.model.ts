662f69154dbb7f0420be5fb53bfd53db506eb596
Damien (Tung) Leu <leudeptrai@gmail.com>
2021-11-18 02:12:53 -0500

    [TS-189] Removed unnecessary fields in userInfo (#18)

--- src/db/models/userInfo.model.ts
+++ src/db/models/userInfo.model.ts
@@ -4,22 +4,16 @@ import { UserDocument } from "./user.model";
 export interface UserInfo extends mongoose.Document {
 	firstname: string;
 	lastname: string;
-	username: string;
 	email: UserDocument["email"];
 	portfolio: any[];
 	userId: UserDocument["_id"];
-	followers: UserDocument["_id"][];
-	following: UserDocument["_id"][];
 }
 
 const UserSchema: Schema = new Schema({
 	firstname: { type: String, required: true },
 	lastname: { type: String, required: true },
-	username: {type: String },
 	email: { type: String, ref: "UserDocument" },
 	userId: { type: mongoose.Schema.Types.ObjectId, ref: "UserDocument" },
-	followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "UserDocument", default: [] }],
-	following: [{ type: mongoose.Schema.Types.ObjectId, ref: "UserDocument", default: [] }],
 });
 
 export default mongoose.model<UserInfo>("UserInfo", UserSchema);
