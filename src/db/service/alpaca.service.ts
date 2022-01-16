fef5ed902e9f27bbd7e02c8e2d52f23bef74ba2d
Tung Leu <leudeptrai@gmail.com>
2022-01-15 23:12:14 -0500

    [TS-244] Fixed code smell

--- src/db/service/alpaca.service.ts
+++ src/db/service/alpaca.service.ts
@@ -1,15 +1,7 @@
 import axios from "../../utils/axios/axios.v1";
-import { Request, Response } from "express";
 import { messages } from "../../db/messages";
 export default class AlpacaService {
-	constructor() {}
-
-
-	async getInfo(
-		endPoint: string,
-		infoType: string,
-		alpacaToken: string
-	) {
+	async getInfo(endPoint: string, infoType: string, alpacaToken: string) {
 		try {
 			const { data } = await axios.get(endPoint, {
 				headers: { Authorization: `Bearer ` + alpacaToken },
@@ -19,6 +11,5 @@ export default class AlpacaService {
 		} catch (error: any) {
 			return messages.internalError(error.response.data.message);
 		}
-		
 	}
 }
