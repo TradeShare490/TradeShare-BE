3bd20b2a28a032c54828b9493574b39990031fcf
bowe99 <kevinbowe1999@gmail.com>
2022-01-17 14:43:59 -0500

    [TS-245] Separate controller into testable service

--- src/controllers/search.controller.ts
+++ src/controllers/search.controller.ts
@@ -2,30 +2,18 @@ import { Request, Response } from "express";
 import mongoose from "mongoose";
 import axios from "../utils/axios/axios.v1";
 import { messages } from "../db/messages";
+import SearchService from "../db/service/search.service";
 
-class SearchController {    
+class SearchController {   
+	private searchService: SearchService;
+	constructor() {
+		this.searchService = new SearchService();
+	}
 	async getStocksSuggestions(req: Request, res: Response) {
 		const searchString = req.params.searchString;
-        const url = 'https://www.alphavantage.co/query?function=SYMBOL_SEARCH'
-		let searchResults = {};
+        const url = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH`		
 		
-		try{
-            if(process.env.ALPHAVANTAGE_API_KEY == undefined) {
-                throw new Error("Alphavantage api key is undefined");
-            }
-			const {data}= await axios.get(`${url}&keywords=${searchString}&apikey=${process.env.ALPHAVANTAGE_API_KEY}`, {
-				headers: { 'User-Agent': 'request' },
-			});
-			return res.send(messages.successMessage("success", "searchResult", data))
-		}
-		catch (error: any) {
-            if(error.response == undefined) {
-                return res.send(messages.internalError(error.message));
-            } else {
-                return res.send(messages.internalError(error.response.data.message))
-            }
-			 
-		}
+		return res.send(await this.searchService.getStockSuggestions(url, searchString));
 	}
 }
 
