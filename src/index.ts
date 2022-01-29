43ec07c16c246a19434f4e2b24b7916ee7174a2a
Ken Nguyen <kenn.tnguyen@gmail.com>
2022-01-29 15:17:00 -0500

    [TS-406] updated comments in index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,16 +13,20 @@ const PORT = process.env.PORT || 5000;
 const HOST = process.env.HOST || "http://localhost";
 const app: Express = express();
 
+// loading swagger
 var path = require("path");
 var swagger_path = path.resolve(__dirname, "../swagger.yaml");
 const swaggerDocument = YAML.load(swagger_path);
 app.use("/swagger", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
+// loading middlewares
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(deserializeUser);
+
+// loading routes
 require("./routes/v1")(app);
 
 const server = app.listen(PORT, async () => {
