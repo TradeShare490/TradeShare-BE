d18f307427e11274b26c87746a4dd19a6169fbd9
The Kien (Ken) Nguyen <60043570+kennguyen0303@users.noreply.github.com>
2022-01-30 17:09:20 -0500

    [TS-406] added swagger UI (#34)
    * [TS-319] update swagger ui
    
    * [TS-406] updated swagger url
    
    * [TS-406] updated comments in index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,28 @@ import cors from "cors";
 import mongoInstance from "./db/connect";
 import deserializeUser from "./middleware/deserializeUser";
 import neo4j from "./db/neo4j/Neo4jInstance";
+import swaggerUI from "swagger-ui-express";
+import YAML from "yamljs";
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 const HOST = process.env.HOST || "http://localhost";
 const app: Express = express();
 
+// loading swagger
+var path = require("path");
+var swagger_path = path.resolve(__dirname, "../swagger.yaml");
+const swaggerDocument = YAML.load(swagger_path);
+app.use("/swagger", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+// loading middlewares
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(deserializeUser);
+
+// loading routes
 require("./routes/v1")(app);
 
 const server = app.listen(PORT, async () => {
