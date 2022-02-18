7f3cd3d514d1d91004b0e6f1b9bfa2f11db7bc34
Damien (Tung) Leu <leudeptrai@gmail.com>
2022-02-17 23:32:40 -0500

    [TS-380][TS-422][TS-398][TS-382] Added conversation and message models and their operations (#38)
    * [TS-380] Added conversation and message models and their operations
    
    * [TS-380] fix code :poop:
    
    * [TS-380] fix code :poop:
    
    * [TS-380] Added timestamp and latest message in conversation
    
    * [TS-380] Addressed code :poop:
    
    * [TS-380] Fixed typo
    
    * [TS-380] Addressed comments

---
+++ src/db/service/ConversationService.ts
@@ -0,0 +1,58 @@
+import ConversationModel, { Conversation } from "../models/conversation.model";
+import { FilterQuery, QueryOptions, UpdateQuery } from "mongoose";
+import { messages } from "../messages";
+
+export default class ConversationService {
+	async createConversation(members: Array<string>) {
+		try {
+			const conversation = await ConversationModel.create({ members: members });
+			return messages.successMessage("Conversation is created", "conversation", conversation);
+		} catch (error: any) {
+			return messages.internalError(error.message);
+		}
+	}
+
+	async getConversations(member: string) {
+		try {
+			const conversations = await ConversationModel.find({
+				members: { $in: [member] },
+			});
+			return messages.successMessage("Conversations are found", "conversations", conversations);
+		} catch (error: any) {
+			return messages.internalError(error.message);
+		}
+	}
+
+	async findConversation(query: FilterQuery<Conversation>) {
+		try {
+			return await ConversationModel.findOne(query);
+		} catch (error: any) {
+			/* istanbul ignore next  */
+			return messages.internalError(error.message);
+		}
+	}
+
+	async updateConversation(
+		query: FilterQuery<Conversation>,
+		update: UpdateQuery<Conversation>,
+		options: QueryOptions
+	) {
+		try {
+			return await ConversationModel.findOneAndUpdate(query, update, options);
+		} catch (error: any) {
+			/* istanbul ignore next */
+			return messages.internalError(error.message);
+		}
+	}
+
+	async deleteConversation(id: string) {
+		try {
+			const response = await ConversationModel.deleteOne({
+				_id: id,
+			});
+			return messages.successMessage("Conversation deleted", "deletedCount", response.deletedCount);
+		} catch (error: any) {
+			return messages.internalError(error.message);
+		}
+	}
+}
---
+++ src/db/service/MessageService.ts
@@ -0,0 +1,25 @@
+import MessageModel from "../models/message.model";
+import { messages } from "../messages";
+import mongoose from "mongoose";
+
+export default class MessageService {
+	async createMessage(sender: string, message: string, conversationId: mongoose.Types.ObjectId) {
+		const createMessage = await MessageModel.create({
+			sender: sender,
+			message: message,
+			conversationId: conversationId,
+		});
+		return messages.createdMessage("Message is created", "message", createMessage);
+	}
+
+	async getMessage(conversationId: mongoose.Types.ObjectId) {
+		try {
+			const response = await MessageModel.find({
+				conversationId: conversationId,
+			});
+			return messages.successMessage("Messages are found", "messages", response);
+		} catch (error: any) {
+			return messages.internalError(error.message);
+		}
+	}
+}
