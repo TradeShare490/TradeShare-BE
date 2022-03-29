5f8a987fd739bbc8a2a8587b2510ddc42c371487
Tung Leu <leudeptrai@gmail.com>
2022-03-28 20:38:18 -0400

    fixed test

--- src/tests/9_Message.test.ts
+++ src/tests/9_Message.test.ts
@@ -7,7 +7,7 @@ import FollowService from '../modules/follows/FollowService'
 import UserService from '../db/service/UserService'
 import UserCollection from '../db/models/user.model'
 
-describe('Message service can', () => {
+describe.only('Message service can', () => {
 	let conversationService: ConversationService
 	let mockedConversation: Conversation
 	let messageService: MessageService
@@ -26,14 +26,14 @@ describe('Message service can', () => {
 		expect(userService).not.equal(undefined)
 		followService = new FollowService()
 		expect(followService).not.equal(undefined)
-		const response = await conversationService.createConversation(['user1', 'user2'], ['User 1', 'User 2'])
+		const response = await conversationService.createConversation(['user2', 'user1'], ['User 2', 'User 1'])
 		uId = (await userService.getUser({ username: 'user1' })).user._id.toJSON()
 		console.log(uId)
 		mockedConversation = response.conversation
 	})
 
 	it('create a new message', async () => {
-		const response = await messageService.createMessage('user1', 'abcdef', mockedConversation._id)
+		const response = await messageService.createMessage('user2', 'abcdef', mockedConversation._id)
 		expect(response).to.have.property('message')
 		expect(response.status).equals(201)
 		expect(response.success).equals(true)
