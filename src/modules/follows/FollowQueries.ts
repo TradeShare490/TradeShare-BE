7bff9c70824f27bfd3177bf2183b40ac9f77f9c5
kennguyen0303 <kenn.tnguyen@gmail.com>
2022-03-29 04:04:26 -0400

    [TS-485] added queries

--- src/modules/follows/FollowQueries.ts
+++ src/modules/follows/FollowQueries.ts
@@ -1,11 +1,11 @@
 export const followQueries = {
-        CREATE_RELATIONSHIP: `
+	CREATE_RELATIONSHIP: `
                 MERGE (src: User{userId: $src})
                 MERGE (target: User{userId: $target})
                 MERGE p=(src)-[r:Follows{isPending: $isPending}]->(target)
                 return count(p) as numOfPath, ID(r) as relId
         `,
-        GET_RELATIONSHION_BETWEEN_USERS: `
+	GET_RELATIONSHION_BETWEEN_USERS: `
                 MATCH p=(src: User{userId: $src})-[r:Follows]->(target: User{userId: $target})
                 WITH p
                 RETURN 
@@ -14,22 +14,38 @@ export const followQueries = {
                 ELSE false
                 END AS relExists
         `,
-        GET_FOLLOWS_FOR_USER: `
+	GET_FOLLOWS_FOR_USER: `
                 MATCH (n:User{userId: $userId})-[r:Follows{isPending: false}]->(m:User)
                 RETURN m.userId as followId
         `,
-        GET_FOLLOWERS_FOR_USER: `
+	GET_FOLLOWERS_FOR_USER: `
                 MATCH (n:User)-[r:Follows{isPending: false}]->(m:User{userId: $userId})
                 RETURN n.userId as followerId
         `,
-        DELETE_RELATIONSHIP_BY_USER_ID: `
+	DELETE_RELATIONSHIP_BY_USER_ID: `
                 MATCH (src: User{userId: $src})-[r:Follows{isPending:$isPending}]->(target: User{userId: $target})
                 DELETE r
                 RETURN COUNT(*) as numbDeleted
         `,
-        DELETE_USER_NODE_BY_ID: `
+	DELETE_USER_NODE_BY_ID: `
                 MATCH (n:User{userId: $userId})
                 DETACH DELETE n
                 RETURN COUNT(*) as numbDeleted                 
+        `,
+	SET_PENDING_REQUEST: `
+                MATCH (src:User)-[r:Follows]->(target: User)
+                WHERE ID(r) = $relId
+                SET r.isPending = $isPending
+                RETURN COUNT(*) as numbModified 
+        `,
+	GET_PENDING_REQUESTS_FOR_USER: `
+                MATCH (n:User)-[r:Follows{isPending: true}]->(m:User{userId: $userId})
+                RETURN n.userId as senderId, ID(r) as relId
+        `,
+	DELELTE_REQUEST_BY_ID: `
+                MATCH (src:User)-[r:Follows]->(target: User)
+                WHERE ID(r) = $relId
+                DELETE r
+                RETURN COUNT(*) as numbDeleted 
         `
 }
